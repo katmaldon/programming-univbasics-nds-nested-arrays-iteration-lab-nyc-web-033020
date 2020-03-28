1286652872ee2f1357c3df9a8c85907856591248
Kat Maldon <katharinamaldon@gmail.com>
2020-03-28 22:49:22 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -1,18 +1,14 @@
 def join_ingredients(src)
-  
   new_array = []
   	  i = 0
   while i < src.length do 
     new_array.push("I love #{src[i][0]} and #{src[i][1]} on my pizza")
-  	    i += 1
+  i += 1
   end
-  
   new_array
-    
 end
 
 def find_greater_pair(src)
-  
   new_array = []
   i = 0 
   
@@ -24,13 +20,18 @@ def find_greater_pair(src)
 end
 
 def total_even_pairs(src)
-  total = 0
-  i = 0
-  
-  while i < src.length do
-    if (src[i][0] % 2 == 0) && (src[i][1] % 2 == 0)
-      i += 1
-    end	  
-  end
-  total
+  src = [ :n1[5, 4], :n2[6, 9], :n3[1,1] ]
+  if  n1%1==0 && n1.to_i.even?
+    total1 = n1[0]+n1[1]
+    puts total1
+    elsif 
+    n%1==0 && n2.to_i.even?
+    total2 = n2[0]+n2[1]
+    puts total2
+    elsif 
+    n%1==0 && n3.to_i.even?
+    total3 = n3[0]+n1[1]
+    puts total3
+  else NIL
+
 end
